7e98a17656b29c3dfb5877fc665c3ac70f27b6a9
Chris Morrell <github@rchy.net>
2013-05-29 23:44:56 -0400

    Initial transition to PHP4

--- swiftype.php
+++ swiftype.php
@@ -1,14 +1,55 @@
 <?php
-namespace Swiftype;
+
+// json_last_error polyfill
+if (!function_exists('json_last_error')) {
+	function json_last_error() {
+		global $_polyfill__json_last_error;
+		if isset($_polyfill__json_last_error) {
+			return $_polyfill__json_last_error;
+		} else if (defined('JSON_ERROR_NONE')) {
+			return JSON_ERROR_NONE;
+		} else {
+			return 0;
+		}
+	}
+}
+
+// json_encode polyfill using Services_JSON
+if( !function_exists('json_encode') ) {
+	require_once 'JSON.php';
+    function json_encode($data) {
+    	global $_polyfill__json_last_error;
+        $json = new Services_JSON();
+        $encoded = $json->encode($data);
+        if (Services_JSON::isError($decoded)) {
+        	$_polyfill__json_last_error = 4;
+        }
+        return $encoded;
+    }
+}
+ 
+// json_decode polyfill using Services_JSON
+if( !function_exists('json_decode') ) {
+	require_once 'JSON.php';
+    function json_decode($data) {
+    	global $_polyfill__json_last_error;
+        $json = new Services_JSON();
+        $decoded = $json->decode($data);
+        if (Services_JSON::isError($decoded)) {
+        	$_polyfill__json_last_error = 4;
+        }
+        return $decoded;
+    }
+}
 
 class SwiftypeClient {
-	private $username;
-	private $password;
-	private $api_key;
-	private $host;
-	private $api_base_path;
+	var $username;
+	var $password;
+	var $api_key;
+	var $host;
+	var $api_base_path;
 
-	public function __construct($username = null, $password = null, $api_key = null, $host = 'http://api.swiftype.com', $api_base_path = '/api/v1/') {
+	function SwiftypeClient($username = null, $password = null, $api_key = null, $host = 'http://api.swiftype.com', $api_base_path = '/api/v1/') {
 		$this->username = $username;
 		$this->password = $password;
 		$this->api_key = $api_key;
@@ -16,23 +57,23 @@ class SwiftypeClient {
 		$this->api_base_path = $api_base_path;
 
 		if(!function_exists('curl_init')){
-			throw new \Exception('Swiftype requires the CURL PHP extension.');
+			throw new Exception('Swiftype requires the CURL PHP extension.');
 		}
 
 		if(!function_exists('json_decode')){
-  			throw new \Exception('Swiftype requires the JSON PHP extension.');
+  			throw new Exception('Swiftype requires the JSON PHP extension.');
 		}
 	}
 
-	public function engines() {
+	function engines() {
 		return $this->get($this->engines_path());
 	}
 
-	public function engine($engine_id) {
+	function engine($engine_id) {
 		return $this->get($this->engine_path($engine_id));
 	}
 
-	public function create_engine($engine_id) {
+	function create_engine($engine_id) {
 		$engine = array(
 			'engine' => array(
 				'name' => $engine_id
@@ -42,19 +83,19 @@ class SwiftypeClient {
 		return $this->post($this->engines_path(), array(), $engine);
 	}
 
-	public function destroy_engine($engine_id) {
+	function destroy_engine($engine_id) {
 		return $this->delete($this->engine_path($engine_id));
 	}
 
-	public function document_types($engine_id) {
+	function document_types($engine_id) {
 		return $this->get($this->document_types_path($engine_id));
 	}
 
-	public function document_type($engine_id, $document_type_id) {
+	function document_type($engine_id, $document_type_id) {
 		return $this->get($this->document_type_path($engine_id, $document_type_id));
 	}
 
-	public function create_document_type($engine_id, $document_type_id) {
+	function create_document_type($engine_id, $document_type_id) {
 		$document_type = array(
 			'document_type' => array(
 				'name' => $document_type_id
@@ -63,59 +104,59 @@ class SwiftypeClient {
 		return $this->post($this->document_types_path($engine_id), array(), $document_type);
 	}
 
-	public function destroy_document_type($engine_id, $document_type_id) {
+	function destroy_document_type($engine_id, $document_type_id) {
 		return $this->delete($this->document_type_path($engine_id, $document_type_id));
 	}
 
-	public function documents($engine_id, $document_type_id) {
+	function documents($engine_id, $document_type_id) {
 		return $this->get($this->documents_path($engine_id, $document_type_id));
 	}
 
-	public function document($engine_id, $document_type_id, $document_id) {
+	function document($engine_id, $document_type_id, $document_id) {
 		return $this->get($this->document_path($engine_id, $document_type_id, $document_id));
 	}
 
-	public function create_document($engine_id, $document_type_id, $document = array()) {
+	function create_document($engine_id, $document_type_id, $document = array()) {
 		return $this->post($this->documents_path($engine_id, $document_type_id), array(), array('document' => $document));
 	}
 
-	public function create_or_update_document($engine_id, $document_type_id, $document = array()) {
+	function create_or_update_document($engine_id, $document_type_id, $document = array()) {
 		return $this->post($this->documents_path($engine_id, $document_type_id).'/create_or_update', array(), array('document' => $document));
 	}
 
-	public function update_document($engine_id, $document_type_id, $document_id, $fields = array()) {
+	function update_document($engine_id, $document_type_id, $document_id, $fields = array()) {
 		return $this->put($this->document_path($engine_id, $document_type_id, $document_id).'/update_fields', array(), array('fields' => $fields));
 	}
 
-	public function update_documents($engine_id, $document_type_id, $documents = array()) {
+	function update_documents($engine_id, $document_type_id, $documents = array()) {
 		return $this->put($this->documents_path($engine_id, $document_type_id).'/bulk_update', array(), array('documents' => $documents));
 	}
 
-	public function create_or_update_documents($engine_id, $document_type_id, $documents = array()) {
+	function create_or_update_documents($engine_id, $document_type_id, $documents = array()) {
 		return $this->post($this->documents_path($engine_id, $document_type_id).'/bulk_create_or_update', array(), array('documents' => $documents));
 	}
 
-	public function destroy_document($engine_id, $document_type_id, $document_id) {
+	function destroy_document($engine_id, $document_type_id, $document_id) {
 		return $this->delete($this->document_path($engine_id, $document_type_id, $document_id));
 	}
 
-	public function destroy_documents($engine_id, $document_type_id, $document_ids = array()) {
+	function destroy_documents($engine_id, $document_type_id, $document_ids = array()) {
 		return $this->post($this->documents_path($engine_id, $document_type_id).'/bulk_destroy', array(), array('documents' => $document_ids));
 	}
 
-	public function search($engine_id, $document_type_id=null, $query, $options = array()) {
+	function search($engine_id, $document_type_id=null, $query, $options = array()) {
 		$query_string = array('q' => $query);
 		$full_query = array_merge($query_string, $options);
 		return $this->post($this->search_path($engine_id, $document_type_id), array(), $full_query);
 	}
 
-	public function suggest($engine_id, $document_type_id=null, $query, $options = array()) {
+	function suggest($engine_id, $document_type_id=null, $query, $options = array()) {
 		$query_string = array('q' => $query);
 		$full_query = array_merge($query_string, $options);
 		return $this->post($this->suggest_path($engine_id, $document_type_id), array(), $full_query);
 	}
 
-	private function search_path($engine_id, $document_type_id = null) {
+	function search_path($engine_id, $document_type_id = null) {
 		if ($document_type_id === null) {
 			return 'engines/'.$engine_id.'/search';
 		} else {
@@ -123,7 +164,7 @@ class SwiftypeClient {
 		}
 	}
 
-	private function suggest_path($engine_id, $document_type_id = null) {
+	function suggest_path($engine_id, $document_type_id = null) {
 		if ($document_type_id === null) {
 			return 'engines/'.$engine_id.'/suggest';
 		} else {
@@ -131,47 +172,47 @@ class SwiftypeClient {
 		}
 	}
 
-	private function engines_path() {
+	function engines_path() {
 		return 'engines';
 	}
 
-	private function engine_path($engine_id) {
+	function engine_path($engine_id) {
 		return 'engines/'.$engine_id;
 	}
 
-	private function document_type_path($engine_id, $document_type_id) {
+	function document_type_path($engine_id, $document_type_id) {
 		return $this->engine_path($engine_id).'/document_types/'.$document_type_id;
 	}
 
-	private function document_types_path($engine_id) {
+	function document_types_path($engine_id) {
 		return $this->engine_path($engine_id).'/document_types';
 	}
 
-	private function document_path($engine_id, $document_type_id, $document_id) {
+	function document_path($engine_id, $document_type_id, $document_id) {
 		return $this->document_type_path($engine_id, $document_type_id).'/documents/'.$document_id;
 	}
 
-	private function documents_path($engine_id, $document_type_id) {
+	function documents_path($engine_id, $document_type_id) {
 		return $this->document_type_path($engine_id, $document_type_id).'/documents';
 	}
 
-	private function get($path, $params = array(), $data = array()) {
+	function get($path, $params = array(), $data = array()) {
 		return $this->request('GET', $path, $params, $data);
 	}
 
-	private function post($path, $params = array(), $data = array()) {
+	function post($path, $params = array(), $data = array()) {
 		return $this->request('POST', $path, $params, $data);
 	}
 
-	private function delete($path, $params = array(), $data = array()) {
+	function delete($path, $params = array(), $data = array()) {
 		return $this->request('DELETE', $path, $params, $data);
 	}
 
-	private function put($path, $params = array(), $data = array()) {
+	function put($path, $params = array(), $data = array()) {
 		return $this->request('PUT', $path, $params, $data);
 	}
 
-	private function request($method, $path, $params = array(), $data = array()) {
+	function request($method, $path, $params = array(), $data = array()) {
 		//Final URL
 		$full_path = $this->host.$this->api_base_path.$path.'.json';
 
@@ -181,7 +222,15 @@ class SwiftypeClient {
 		}
 
 		//Build the query string
-		$query = http_build_query($params);
+		$query = false;
+		if (!empty($params)) {
+			$query = '';
+			foreach ($params as $key => $value) {
+				$query = $key . '=' . urlencode($value) . '&';
+			}
+			$query = rtrim($query, '&');
+		}
+
 
 		if ($query) {
 			$full_path .= '?' . $query;
@@ -194,7 +243,7 @@ class SwiftypeClient {
 		if ($this->api_key === null && $this->username != null && $this->password != null) {
 			curl_setopt($request, CURLOPT_USERPWD, $this->username.':'.$this->password);
 		} elseif ($this->api_key === null && $this->username === null && $this->password === null) {
-			throw new \Exception('Authorization required.');
+			throw new Exception('Authorization required.');
 		}
 
 		//Return the output instead of printing it
@@ -219,10 +268,10 @@ class SwiftypeClient {
 		$error = curl_error($request);
 
 		if ($error) {
-	  		throw new \Exception("Sending message failed. Error: ". $error);
+	  		throw new Exception("Sending message failed. Error: ". $error);
 		}
 
-		$http_status = (int)curl_getinfo($request,CURLINFO_HTTP_CODE);
+		$http_status = intval(curl_getinfo($request,CURLINFO_HTTP_CODE));
 		curl_close($request);
 
 		//Any 2XX HTTP codes mean that the request worked
@@ -261,12 +310,12 @@ class SwiftypeClient {
 					return array('status' => $http_status);
 				}
 			} else {
-				throw new \Exception('The JSON response could not be parsed: '.$error. '\n'.$response);
+				throw new Exception('The JSON response could not be parsed: '.$error. '\n'.$response);
 			}
 		} elseif ($http_status === 401) {
-			throw new \Exception('Authorization required.');
+			throw new Exception('Authorization required.');
 		} else {
-	  		throw new \Exception("Couldn't send message, got response code: ". $http_status. " response: ".$response);
+	  		throw new Exception("Couldn't send message, got response code: ". $http_status. " response: ".$response);
 		}
 	}
 }
